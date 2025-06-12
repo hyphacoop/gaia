406442e1d3f8e29f1e09ee5152af7f0e49e0108d
violet <violet@hypha.coop>
2025-06-12 10:48:08 -0400

    bump interchaintest

--- tests/interchain/chainsuite/chain_ics.go
+++ tests/interchain/chainsuite/chain_ics.go
@@ -16,6 +16,7 @@ import (
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
 	"go.uber.org/multierr"
+	"golang.org/x/mod/semver"
 
 	clienttypes "github.com/cosmos/ibc-go/v10/modules/core/02-client/types"
 	ccvclient "github.com/cosmos/interchain-security/v7/x/ccv/provider/client"
@@ -131,7 +132,10 @@ func (p *Chain) AddConsumerChain(ctx context.Context, relayer *Relayer, config C
 	if config.Spec.InterchainSecurityConfig.ICSImageRepo == "" {
 		config.Spec.InterchainSecurityConfig.ICSImageRepo = "ghcr.io/hyphacoop/ics"
 	}
-	// providerICS := p.GetNode().ICSVersion(ctx)
+	providerICS := p.GetNode().ICSVersion(ctx)
+	if semver.MajorMinor(providerICS) == semver.MajorMinor("v7.0.0") {
+		config.Spec.InterchainSecurityConfig.ProviderVerOverride = "v7.0.0"
+	}
 	// if config.Spec.InterchainSecurityConfig.ConsumerVerOverride == "" {
 	// 	// This will disable the genesis transform
 	// 	config.Spec.InterchainSecurityConfig.ConsumerVerOverride = providerICS
